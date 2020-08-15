4cb6b7f1dc084f3cd2e9c869ba77ca361d3a2ab8
saraezzooff <saraezzooff@gmail.com>
2020-08-15 15:47:42 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import TodoList from './TodoList';
 import './App.css';
+import logo from './delete.png';
 
 function App() {
+
+const [todo, setTodo] = useState("");
+const [todosList, setTodosList] = useState([]);
+const [remove, setRemove] = useState(true);
+
+const AddTodos = e =>{
+  e.preventDefault();
+  //alert(todo);
+  console.log(todosList);
+  
+  setTodosList([...todosList, {id : todosList.length + 1, name : todo}]);
+  setTodo("");
+  
+  
+}
+
+const removeBlock = (id)=> {
+
+    setRemove(false);
+    const filterTodo = todosList.filter(t => t.id !== id);
+    setTodosList(filterTodo);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header className="App">
+    
+      <form id="my-form" onSubmit={AddTodos}>
+         <input 
+         type="text" 
+         placeholder="Enter Something TODO"
+         name="todo"
+         value={todo}
+         onChange={e => {
+          setTodo(e.target.value)
+        //  console.log(e.target.value);
+        }}
+          />
+         <button type="submit">Add</button>
+      </form>
+   
+      {
+          
+         todosList.map(todo =>{
+         
+            return(
+              <div className="list"  key = {todo.id}>
+                 <p>{todo.name}</p>
+                 <img alt="" src={logo}  onClick={(e)=>removeBlock(todo.id)}/>
+              </div>
+            );
+
+          
+        
+            })
+        }
+ 
+  
+    </header>
+   
+    </>
+
+
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react';
+import './TodoList.css';
+
+function TodoList(props){
+
+    const TodoList = props.TodoList;
+
+return(
+
+    <h1>hello App</h1>
+);
+
+}
+
+export default TodoList;
